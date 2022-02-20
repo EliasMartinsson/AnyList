244793fc4b4fc79e6f434b839028ca05f60b6f36
Elias Martinsson <elias.martinsson@elev.ga.ntig.se>
2022-02-20 23:26:06 +0100

    Finished header and added some javascript functionality to it

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
- 
+ console.log("Hello world!");
+ window.onscroll = function() {stickyHeader()};
+
+ var header = document.getElementById("stickyHeader");
+
+ var sticky = header.offsetTop;
+
+ function stickyHeader() {
+     if(window.pageYOffset > sticky){
+         header.classList.add("sticky");
+     }
+     else{
+         header.classList.remove("sticky");
+     }
+ }
