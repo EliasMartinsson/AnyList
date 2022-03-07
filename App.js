9ec67a2208c3dfe78661480d1b38b84f01f4883c
Elias Martinsson <elias.martinsson@elev.ga.ntig.se>
2022-03-07 23:56:12 +0100

    Changes i worked on during the break

--- App.js
+++ App.js
@@ -6,10 +6,22 @@ let dropDownLinks = document.querySelectorAll(".top-links button");
 dropDownLinks.forEach((dropDownLink) => {
   dropDownLink.addEventListener("click", clickedLink);
 });
-
+//Fråga Sebastian om hjälp P.S lägg till så att den klickade länken byter färg
 function clickedLink(event) {
   dropDownIsOpen = true;
-  currentDropDown = event.target.parent.getAttribute("data-drop-down");
+  currentDropDown = event.target.parent.getAttribute("data-dropdown");
+  if(currentDropDown === "link-dropdown"){
+
+  }
+  if(currentDropDown === "schedule-dropdown"){
+
+  }
+  if(currentDropDown === "tierlist-dropdown"){
+    
+  }
+  if(currentDropDown === "community-dropdown"){
+
+  }
 }
 
 console.log(dropDownLinks);
