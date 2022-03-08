2ee9b48db407b6a5899b851041970def6121c99e
Elias <elias.martinsson@elev.ga.ntig.se>
2022-03-08 18:08:36 +0100

    Minor

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
 let currentDropDown = 0;
 let dropDownIsOpen = false;
 
+/*
+exempel på setAttrbite och getAttribute: 
+let myButton = document.querySelector("button");
+let buttonDropdown = myButton.getAttribute("data-dropdown");
+buttonDropdown = "hej";
+myButton.setAttrbute("data-dropdown", buttonDropdown); */
+
 let dropDownLinks = document.querySelectorAll(".top-links button");
 
 dropDownLinks.forEach((dropDownLink) => {
   dropDownLink.addEventListener("click", clickedLink);
 });
-//Fråga Sebastian om hjälp P.S lägg till så att den klickade länken byter färg
+
 function clickedLink(event) {
   dropDownIsOpen = true;
   currentDropDown = event.target.parent.getAttribute("data-dropdown");
-  if(currentDropDown === "link-dropdown"){
-
-  }
-  if(currentDropDown === "schedule-dropdown"){
-
-  }
-  if(currentDropDown === "tierlist-dropdown"){
-    
-  }
-  if(currentDropDown === "community-dropdown"){
-
-  }
+  let dropDownElement = document.querySelector("." + currentDropDown);
+  dropDownElement.classList.toggle("visible");
 }
 
 console.log(dropDownLinks);
